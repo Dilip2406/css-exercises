74cad43e713bf6bb1acad2937ea6323a7105d4bf
Dilip2406 <dilipsinghm.mca22@rvce.edu.in>
2023-02-26 12:02:37 +0530

    update css

---
+++ foundations/A_Restoring_Three_Numbers.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+int arr[];
+for(int i=0;i<4;i++){ 
+    cin>>arr[i];
+}
+int max=INT_MAX;
+for(int i=0;i<n;i++){
+    if(max>arr[i])
+    {
+        max=arr[i];
+    }
+}
+int r1,r2,r3;
+for(int i=0;i<4;i++){
+    max-arr[i]
+}
+
